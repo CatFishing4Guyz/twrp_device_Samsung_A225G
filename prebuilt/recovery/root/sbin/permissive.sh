257b328f5c19a97a6ff5376cbb5fd78fe15acdfa
Monkeys30 <47145954+Monkeys30@users.noreply.github.com>
2022-01-04 15:49:11 +0800

    Update permissive.sh

--- prebuilt/recovery/root/sbin/permissive.sh
+++ prebuilt/recovery/root/sbin/permissive.sh
@@ -5,4 +5,4 @@ setenforce 0
 # Get your device's block path where "system", "recovery", etc. lives. 
 # That can be "/dev/block/bootdevice/by-name" or something like that. 
 mkdir -p /dev/block/platform/mtk-msdc.0/by-name/ 
-busybox mount -o bind /dev/block/platform/bootdevice/by-name/ /dev/block/by-name
+busybox mount -o bind /dev/block/platform/mtk-msdc.0/by-name/ /dev/block/by-name/
